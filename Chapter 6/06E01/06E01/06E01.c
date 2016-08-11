bba8513e3d3a3c2d5d352bb6cc6792c33a1ddbe9
iforgot120 <iforgot120@gmail.com>
2016-08-10 22:37:55 -0500

    06E01: Can now parse comments, quotes, preprocessor directives
    .... sort of. Bug with quotes: enters quotes, then exits immediately
    based on the same '"' character. Will fix tomorrow.
    
    Also came up with this cool commit summary standard of prefixing the
    commit summary with the problem number(s) worked on.

--- Chapter 6/06E01/06E01/06E01.c
+++ Chapter 6/06E01/06E01/06E01.c
@@ -1,10 +1,4 @@
 /* 06E01 (p. 136) */
-/*
-TODO:
-- Handle string constants
-- Handle comments
-- Handle preprocessor control lines
-*/
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
@@ -61,11 +55,6 @@ void ungetch(int c);
 
 int buffp = 0;
 char buff[MAXBUFF];
-Bool inComment = False;
-Bool inQuote = False;
-Bool inPreProc = False;
-Bool lineStart = True;
-char comCh;  // '*' or '/'
 
 /* count C keywords */
 int main(void) {
@@ -78,7 +67,9 @@ int main(void) {
                 keytab[n].count++;
     }
     printf("\n");
-    for (n = 0; n < NKEYS; n++)
+    if (NKEYS == 0)
+        printf("No keywords found!\n");
+    else for (n = 0; n < NKEYS; n++)
         if (keytab[n].count > 0)
             printf("%s       \t%4d\n", keytab[n].word, keytab[n].count);
 
@@ -92,55 +83,67 @@ int main(void) {
  * Returns: Bool indiciating if it's okay (True) or not (False)
  */
 Bool ok2Read(char *word) {
-    Bool result = True;
+    static char comCh;  // '*' or '/'
+    static Bool inComment = False;
+    static Bool inQuote = False;
+    static Bool inPreProc = False;
+    static Bool lineStart = True;
     static Bool almostComment = False;
+    static Bool almostNoComment = False;
     static Bool escaped = False;
-    // Check to see if we enter a quote, comment, or preprocessor directive
+    static Bool result = True;  // start assuming result is True
 
-    if (!escaped) {
-        if (word[0] == '\\') {
-            escaped = True;
-            return False;
-        }
-        else if (!inQuote && !inComment) {
-            if (!almostComment && word[0] == '/')
-                almostComment = True;
-            else {
-                if (almostComment && (word[0] == '*' || word[0] == '/')) {
-                    inComment = True;
-                    comCh = word[0];
-                }
-                else if (word[0] == '"')
-                    inQuote = True;
-                else if (lineStart && word[0] == '#')
-                    inPreProc = True;
-                almostComment = False;
+    // Check for characters with special meaning
+    if (inQuote && !escaped && word[0] == '\\') {
+        escaped = True;
+        return False;
+    }
+    // Check to see if we enter a quote, comment, or preprocessor directive
+    if (!inQuote && !inComment) {  // if not in quote or comment
+        if (lineStart && word[0] == '#')
+            inPreProc = True;
+        else if (!almostComment && word[0] == '/')  // maybe enter comment
+            almostComment = True;
+        else {
+            if (almostComment && (word[0] == '*' || word[0] == '/')) {
+                inComment = True;
+                comCh = word[0];
             }
+            else if (word[0] == '"')
+                inQuote = True;
+            almostComment = False;
         }
     }
-    // If we're in any of the three, result = False
-    if (inComment || inQuote || inPreProc)
-        result = False;
+    
     // Check to see if we remain in any of them
-    if (inQuote) {
-        if (!escaped) {
-            if (word[0] == '"')
-                inQuote = False;
-            else if (word[0] == '\n') {
+    if (!result) {  // but only if we haven't just entered them
+        lineStart = False;
+        if (word[0] == '\n' && !escaped) {
+            if (inComment && comCh == '/')
+                inComment = False;
+            if (!inQuote && !inComment) {
                 lineStart = True;
                 inPreProc = False;
-                if (comCh == '/')
-                    inComment = False;
-                if (inQuote)
-                    inQuote = False;
             }
         }
-    }
-    if (inComment) {
-
+        else if (inQuote && !escaped && word[0] == '"')
+            inQuote = False;
+        else if (inComment && comCh == '*') {
+            if (word[0] == '*')
+                almostNoComment = True;
+            else if (almostNoComment) {
+                if (word[0] == '/')
+                    inComment = False;
+                almostNoComment = False;
+            }
+        }
     }
     if (escaped)
         escaped = False;
+
+    // If we're in any of the three, result = False
+    if (inComment || inQuote || inPreProc)
+        result = False;
 
     return result;
 }
@@ -151,7 +154,6 @@ returns: index with the word */
 int binsearch(char *word, struct key tab[], int n) {
     int cond;
     int low, high, mid;
-    printf("test");
     low = 0;
     high = n - 1;
     while (low <= high) {
