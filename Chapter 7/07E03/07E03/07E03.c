dad4da38dc214877843a3c7cd550905ccb38acbc
iforgot120 <iforgot120@gmail.com>
2016-09-06 21:49:47 -0500

    07E03: Finished
    Float number formatting for strings working! Kind of messy, though.

--- Chapter 7/07E03/07E03/07E03.c
+++ Chapter 7/07E03/07E03/07E03.c
@@ -18,11 +18,17 @@ void xputchar(char c, int n);
 
 int main(void) {
     int i = 1;
-    minprintf("\nTest print %i\n", i++);
-    minprintf("Test %s %o\n", "printing", i++);
-    minprintf("Test print %15.10d\n", i++);
+    minprintf("\nTest print %-5i for good luck!\n", i++);
+    minprintf("Test %s %o to start this off\n", "printing", 12 + i++);
+    minprintf("Test print %15.10d for good measure\n", i++);
     minprintf("A test print (#%-4.4f) with floats\n", i++);
-    minprintf("Final %s (num %x)\n\n", "line", i++);
+    minprintf("Final %s (num %x)\n", "line", i++);
+    minprintf("JK, here is #%10d, involving a negative\n", -1 * i++);
+    minprintf("And here is a float: %4.3f that's also negative!\n", -93.242 + i++);
+    minprintf("Here's another.... %-15.12f. TADA!\n", -5220.520 + i++);
+    minprintf("What if I threw an %8.8E in?!\n", -42.2 + i++);
+    minprintf("Or this weird G %-12.12g thing?!\n", 117.120 + i++);
+    minprintf("What about a float <1 such as %4.8f? Weird.\n", 0.3429);
     system("pause");
     return 0;
 }
@@ -109,26 +115,27 @@ void minprintf(char *fmt, ...) {
 
 void formatstring(double v, char t, bool a, int minw, int pre) {
     int numd;
+    int inumd;
     char s[MAXSTRLEN];
     switch (t) {
         case 'd': case 'i': case 'u':
             numd = numdigits(v, 10, pre);
-            if (a && numd < minw)
+            if (!a && (minw > 0 && numd < minw))
                 xputchar(' ', minw - numd);
             _itoa((int)v, s, 10);
             for (int i = 0; s[i] != '\0'; i++)
                 putchar(s[i]);
-            if (!a && numd < minw)
+            if (a && numd < minw)
                 xputchar(' ', minw - numd);
             break;
         case 'o':
             numd = numdigits(v, 8, pre);
-            if (a && numd < minw)
+            if (!a && (minw > 0 && numd < minw))
                 xputchar(' ', minw - numd);
             _itoa((int)v, s, 8);
             for (int i = 0; s[i] != '\0'; i++)
                 putchar(s[i]);
-            if (!a && numd < minw)
+            if (a && numd < minw)
                 xputchar(' ', minw - numd);
             break;
         case 'x': case 'X':
@@ -144,23 +151,85 @@ void formatstring(double v, char t, bool a, int minw, int pre) {
         case 'c':
             putchar((char)v);
             break;
-        case 'f': case 'e': case 'E': case 'g': case 'G':
+        case 'g': case 'G':
             numd = numdigits(v, 10, pre);
-            break;
+            inumd = numdigits((int)v, 10, pre);
+            if (numd - inumd > 4 || inumd >= pre)
+                t = (t == 'g') ? 'e' : 'E';
+            else
+                t = 'f';
+            // continue on to either of the next two...
+        case 'f':
+        {
+            if (t == 'f') {  // run this if it matches 'f'
+                numd = numdigits(v, 10, pre);
+                inumd = numdigits((int)v, 10, pre);
+                if (!a && numd < minw)
+                    xputchar(' ', minw - numd);
+                _itoa((int)v, s, 10);
+                for (int i = 0; s[i] != '\0'; i++)
+                    putchar(s[i]);
+                putchar('.');
+                v -= (int)v;
+                v = (v < 0) ? -v : v;
+                for (int i = 0; i < pre && v != 0.0; i++) {
+                    _itoa((int)(v *= 10), s, 10);
+                    putchar(s[0]);
+                    v -= (int)v;
+                }
+                if (a && numd < minw)
+                    xputchar(' ', minw - numd);
+            }
+        }
+        case 'e': case 'E':
+        {
+            if (t == 'e' || t == 'E') {
+                numd = numdigits(v, 10, pre);
+                inumd = numdigits((int)v, 10, pre);
+                int exp = 0;
+                if (inumd > 1) {
+                    exp = inumd - 1;
+                    if (v < 0) exp--;
+                    for (int i = 0; i < exp; i++, v /= 10.0);
+                }
+                else if (inumd == 0)
+                    for (v; v < 0.0; exp++, v *= 10.0);
+                _itoa((int)v, s, 10);
+                putchar(s[0]);
+                if (v < 0) putchar(s[1]);
+                putchar('.');
+                v -= (int)v;
+                v = (v < 0) ? -v : v;
+                for (int i = 0; i < pre && v != 0.0; i++) {
+                    _itoa((int)(v *= 10), s, 10);
+                    putchar(s[0]);
+                    v -= (int)v;
+                }
+                putchar((t == 'e') ? 'e' : 'E');
+                _itoa(exp, s, 10);
+                for (int i = 0; s[i] != '\0'; i++)
+                    putchar(s[i]);
+                break;
+            }
+        }
         default:
             break;
     }
 }
 
 int numdigits(double v, int base, int pre) {
-    int n;
+    int n = 0;
+    if (v < 0) {
+        n++;
+        v = -v;
+    }
     int i = (int)v;
-    for (n = 0; i > 0; i /= base, n++);  // int truncation
+    for (n; i > 0; i /= base, n++);  // int truncation
     if (v - (int)v != 0) {
         n++;  // for the '.'
         v -= (int)v;  // just the decimal portion
         int d = 0;
-        for (v *= base; v > 0.0 || d < pre; v = (v - (int)v) * base, d++, n++);
+        for (v *= base; v > 0.0 && d < pre; v = (v - (int)v) * base, d++, n++);
     }
     return n;
 }
