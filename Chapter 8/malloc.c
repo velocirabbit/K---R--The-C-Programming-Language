04544d38d1082661736b38e52c96dc282bee5304
iforgot120 <iforgot120@gmail.com>
2017-01-23 19:45:53 -0600

    07E04
    Finished up 07E04. Stopping here since Chapter 8 is mostly Unix OS
    functions. Moving on to the next books!

---
+++ Chapter 8/malloc.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+typedef long Align;  // for alignment to long boundary
+
+typedef union header_ {
+    struct {
+        union header_ *ptr;  // next block if on free list
+        unsigned size;      // size of this block
+    } s;
+    Align x;                // force alignment of block
+} Header;
+
+static Header base;           // empty list to get started
+static Header *freep = NULL;  // start of free list
+
+/* malloc: general-purpose storage allocator */
+void *malloc(unsigned nbytes) {
+    Header *p, *prevp;
+    Header *morecore(unsigned);
+
+    nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+    if ((prevp = freep) == NULL) {  // no free list yet
+        base.s.ptr = freep = prevp = &base;
+        base.s.size = 0;
+    }
+    for (p = prevp->s.ptr; ; prevp = p, p = p->s.ptr) {
+        if (p->s.size >= nunits) {    // big enough
+            if (p->s.size == nunits)  // exactly
+                prevp->s.ptr = p->s.ptr;
+            else {  // allocate tail end
+                p->s.size -= nunits;
+                p += p->s.size;
+                p->s.size = nunits;
+            }
+            freep = prevp;
+            return (void*)(p + 1);
+        }
+        if (p == freep)  // wrapped around free list
+            if ((p = morecore(nunits)) == NULL)
+                return NULL;  // none left
+    }
+}
